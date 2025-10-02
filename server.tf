62d2ea26531bcf98c38910c1817c2f9f769c3f92
RushikeshGhodke <rushikeshghodke7455@gmail.com>
2025-10-02 20:36:13 +0530

    managing the dir structure

--- server.tf
+++ server.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "multi-container-application" {
   ami           = "ami-0360c520857e3138f" 
   instance_type = "t2.micro"
-  key_name      = "my-keypair"
+  key_name      = "tf-multi-container"
 
   tags = {
     Name = "multi-container-application"
